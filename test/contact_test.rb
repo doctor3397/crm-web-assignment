10327cea4de6d861b62f8d7a3cccaffc6da3bd9e
doctor3397 <hs2546@columbia.edu>
2017-01-26 13:34:01 -0500

    Update the tests for the ActiveRecord Model

--- test/contact_test.rb
+++ test/contact_test.rb
@@ -3,8 +3,14 @@ require_relative '../contact'
 
 class TestContact < Minitest::Test
 
+  # Update the tests to work for the Contact ActiveRecord model.
   def setup
-    @contact = Contact.create('Grace', 'Hopper', 'grace@hopper.com', 'computer scientist')
+    # @contact = Contact.create('Grace', 'Hopper', 'grace@hopper.com', 'computer scientist')
+    @contact = Contact.create(
+    first_name: 'Grace',
+    last_name:  'Hopper',
+    email:      'grace@hopper.com',
+    note:       'computer scientist')
   end
 
   def teardown
@@ -20,7 +26,8 @@ class TestContact < Minitest::Test
   end
 
   def test_find_by
-    assert_equal @contact, Contact.find_by('first_name', 'Grace')
+    #assert_equal @contact, Contact.find_by('first_name', 'Grace')
+    assert_equal @contact, Contact.find_by(first_name: 'Grace')
   end
 
   def test_delete_all
@@ -33,7 +40,8 @@ class TestContact < Minitest::Test
   end
 
   def test_update
-    @contact.update('note', 'wrote the first compiler in 1952')
+    @contact.update(note: 'wrote the first compiler in 1952')
+    #@contact.update('note', 'wrote the first compiler in 1952')
     assert_equal 'wrote the first compiler in 1952', @contact.note
   end
 
